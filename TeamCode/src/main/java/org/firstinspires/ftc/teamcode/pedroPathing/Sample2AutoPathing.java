5e2c5d52f69c6e45ea25bb755dbd279ebfb71db5
KiraNotThis <kira.narovlianskaia@gmail.com>
2026-01-16 00:12:19 +0200

    Creating simple autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Sample2AutoPathing.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class Sample2AutoPathing extends OpMode {
+    private Follower follower;
+    private Timer pathTimer, opModeTimer;
+
+    public enum PathState {
+        //START POSITION_END POSITION
+        // DRIVE > MOVEMENT STATE
+        // SHOOT > ATTEMPT TO SCORE THE ARTIFACT
+        DRIVE_STARTPOS_SHOOT_POS,
+        SHOOT_PRELOAD,
+        DRIVE_SHOOTPOS_ENDPOS
+    }
+    PathState pathState;
+
+    private final Pose startPose = new Pose(23.077534791252486,119.93638170974155, Math.toRadians(138));
+    private final Pose shootPose = new Pose(59.54671968190855, 83.88071570576541, Math.toRadians(138));
+    private final Pose endPose = new Pose(81.30417495029822, 106.01391650099406, Math.toRadians(90));
+
+    private PathChain driveStartPosShootPos, driveShootPosEndPos;
+    public void buildPaths() {
+        //put in coordinates from starting pose > ending pose
+        driveStartPosShootPos = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, shootPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), shootPose.getHeading())
+                .build();
+        driveShootPosEndPos = follower.pathBuilder()
+                .addPath(new BezierLine(shootPose, endPose))
+                .setLinearHeadingInterpolation(shootPose.getHeading(), endPose.getHeading())
+                .build();
+    }
+
+
+    public void  statePathUpdate() {
+        switch (pathState){
+            case DRIVE_STARTPOS_SHOOT_POS:
+                follower.followPath(driveStartPosShootPos,true);
+                setPathState(PathState.SHOOT_PRELOAD);
+                break;
+            case SHOOT_PRELOAD:
+                // check is follower done it's path?
+                if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() > 5){
+                    follower.followPath(driveShootPosEndPos, true);
+                    setPathState(PathState.DRIVE_SHOOTPOS_ENDPOS);
+                }
+                break;
+            case DRIVE_SHOOTPOS_ENDPOS:
+                //all done
+                if (!follower.isBusy()){
+                    telemetry.addLine("Done all Paths");
+                }
+            default:
+                telemetry.addLine("No State Commanded");
+                break;
+        }
+    }
+
+    public void setPathState(PathState newState){
+        pathState = newState;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void init(){
+        pathState = PathState.DRIVE_STARTPOS_SHOOT_POS;
+        pathTimer = new Timer();
+        opModeTimer = new Timer();
+        follower = Constants.createFollower(hardwareMap);
+
+        buildPaths();
+        follower.setPose(startPose);
+
+    }
+
+    public void start() {
+        opModeTimer.resetTimer();
+        setPathState(pathState);
+    }
+
+    @Override
+    public void loop(){
+        follower.update();
+        statePathUpdate();
+
+        telemetry.addData("path state", pathState.toString());
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.addData("Path time", pathTimer.getElapsedTimeSeconds());
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/SampleAutoPathing.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.pedropathing.util.Timer;
+
+import kotlin.jvm.internal.PropertyReference0Impl;
+
+@Autonomous
+public class SampleAutoPathing extends OpMode {
+    private Follower follower;
+    private Timer pathTimer, opModeTimer;
+
+    public enum PathState {
+        //START POSITION_END POSITION
+        // DRIVE > MOVEMENT STATE
+        // SHOOT > ATTEMPT TO SCORE THE ARTIFACT
+        DRIVE_STARTPOS_SHOOT_POS,
+        SHOOT_PRELOAD
+    }
+    PathState pathState;
+
+    private final Pose startPose = new Pose(23.077534791252486,119.93638170974155, Math.toRadians(138));
+    private final Pose shootPose = new Pose(59.54671968190855, 83.88071570576541, Math.toRadians(138));
+
+    private PathChain driveStartPosShootPos;
+    public void buildPaths() {
+        //put in coordinates from starting pose > ending pose
+        driveStartPosShootPos = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, shootPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), shootPose.getHeading())
+                .build();
+    }
+
+
+    public void  statePathUpdate() {
+        switch (pathState){
+            case DRIVE_STARTPOS_SHOOT_POS:
+                follower.followPath(driveStartPosShootPos,true);
+                setPathState(PathState.SHOOT_PRELOAD);
+                break;
+            case SHOOT_PRELOAD:
+
+                // check is follower done it's path?
+                if (!follower.isBusy()){
+                    // TODO add logic to shooter
+                    telemetry.addLine("Done Path l");
+                }
+                break;
+            default:
+                telemetry.addLine("No State Commanded");
+                break;
+        }
+    }
+
+    public void setPathState(PathState newState){
+        pathState = newState;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void init(){
+        pathState = PathState.DRIVE_STARTPOS_SHOOT_POS;
+        pathTimer = new Timer();
+        opModeTimer = new Timer();
+        follower = Constants.createFollower(hardwareMap);
+
+        buildPaths();
+        follower.setPose(startPose);
+
+    }
+
+    public void start() {
+        opModeTimer.resetTimer();
+        setPathState(pathState);
+    }
+
+    @Override
+    public void loop(){
+        follower.update();
+        statePathUpdate();
+
+        telemetry.addData("path state", pathState.toString());
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.addData("Path time", pathTimer.getElapsedTimeSeconds());
+    }
+}
